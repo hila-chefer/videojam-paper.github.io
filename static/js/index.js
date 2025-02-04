850dbb7cc14012543542ecf0b708538fe870c047
hila-chefer <hilach70@gmail.com>
2025-02-04 11:40:46 +0200

    .

--- static/js/index.js
+++ static/js/index.js
@@ -19,3 +19,22 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.querySelector('.teaser-container video');
+    const button = document.querySelector('.audio-toggle');
+    const audioOn = document.querySelector('.audio-on');
+    const audioOff = document.querySelector('.audio-off');
+
+    button.addEventListener('click', function() {
+        video.muted = !video.muted;
+        
+        if (video.muted) {
+            audioOff.style.display = 'block';
+            audioOn.style.display = 'none';
+        } else {
+            audioOff.style.display = 'none';
+            audioOn.style.display = 'block';
+        }
+    });
+});
